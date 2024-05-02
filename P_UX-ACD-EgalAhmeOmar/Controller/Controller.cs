e30eeda2790a983433851f6554d6d898e289b379
Omar Egal Ahmed <px55avd@eduvaud.ch>
2024-05-02 16:32:20 +0200

    Version 02.05.2024 : Vue 1-4 faites.

---
+++ P_UX-ACD-EgalAhmeOmar/Controller/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace P_UX_ACD_EgalAhmeOmar.Controller
+{
+    public class Controller
+    {
+        private View _view;
+        private Model.Model _model;
+
+        public Controller(View view, Model.Model model)
+        {
+            _view = view;
+            _model = model;
+            _model.Controller = this;
+            _view.Controller = this;
+        }
+    }
+}
---
+++ P_UX-ACD-EgalAhmeOmar/Views/View3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P_UX_ACD_EgalAhmeOmar.Views
+{
+    public partial class View3 : Form
+    {
+        public View3()
+        {
+            InitializeComponent();
+        }
+
+        private void View3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
